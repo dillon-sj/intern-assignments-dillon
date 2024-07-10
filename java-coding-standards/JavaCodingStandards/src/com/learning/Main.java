fb0f04f34c64c9bd0aafe57590148396dce62cea
dillon-sj <ds.juriansz@gmail.com>
2024-07-10 09:55:36 +0530

    Testing Code refactoring

---
+++ java-coding-standards/JavaCodingStandards/src/com/learning/Main.java
@@ -0,0 +1,20 @@
+package com.learning;
+
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        final Logger logger = Logger.getLogger(Main.class.getName());
+
+        logger.info("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+
+
+
+
+                                    logger.info("i is equal to {}");
+
+
+        }
+    }
+}
