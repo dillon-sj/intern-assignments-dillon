ace97cf77084a2123d641d84c988d32e0f21857b
dillon-sj <ds.juriansz@gmail.com>
2024-07-11 13:28:17 +0530

    Added kotlin assignment task 6

---
+++ kotlin-learning/KotlinAssignments/src/com/dillon/kotlin/assignment6/Main.kt
@@ -0,0 +1,42 @@
+package com.dillon.kotlin.assignment6
+
+
+/**
+ * Copyright 2008-2019 hSenid Software International (Pvt) Limited.
+ * All Rights Reserved.
+ *
+ * These materials are unpublished, proprietary, confidential source code of
+ * hSenid Software International (Pvt) Limited and constitute a TRADE SECRET
+ * of hSenid Software International (Pvt) Limited.
+ *
+ * hSenid Software International (Pvt) Limited retains all title
+ * to and intellectual property rights in these materials.
+ */
+
+
+object MathUtility {
+    fun factorial(n: Int): Long {
+        return if (n <= 1) 1 else n * factorial(n - 1)
+    }
+}
+
+fun main() {
+
+    val book = object {
+
+        val title = "The Catcher in the Rye"
+        val author = "J.D. Salinger"
+
+    }
+
+    println("Book Title: ${book.title}")
+    println("Book Author: ${book.author}")
+
+
+    val number: Int = 5
+    val result = MathUtility.factorial(number)
+    println("Factorial of $number is: $result")
+
+
+}
+
