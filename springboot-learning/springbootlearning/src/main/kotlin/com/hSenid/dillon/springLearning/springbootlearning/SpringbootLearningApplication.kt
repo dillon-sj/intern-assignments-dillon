a67283895d0f058e1c76f86fc212e366134b5e4c
dillon-sj <ds.juriansz@gmail.com>
2024-07-15 14:28:27 +0530

    chore: Add Spring Boot project configuration files

---
+++ springboot-learning/springbootlearning/src/main/kotlin/com/hSenid/dillon/springLearning/springbootlearning/SpringbootLearningApplication.kt
@@ -0,0 +1,11 @@
+package com.hSenid.dillon.springLearning.springbootlearning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootLearningApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootLearningApplication>(*args)
+}
---
+++ springboot-learning/springbootlearning/src/test/kotlin/com/hSenid/dillon/springLearning/springbootlearning/SpringbootLearningApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hSenid.dillon.springLearning.springbootlearning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootLearningApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
