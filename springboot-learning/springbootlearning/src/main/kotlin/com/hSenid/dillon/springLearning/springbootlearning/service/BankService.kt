3d7e6635e79a240bb365cd6e31c49f3d633e1ac0
dillon-sj <ds.juriansz@gmail.com>
2024-07-16 14:20:33 +0530

    feat: Add BankService class for retrieving banks

---
+++ springboot-learning/springbootlearning/src/main/kotlin/com/hSenid/dillon/springLearning/springbootlearning/service/BankService.kt
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2008-2019 hSenid Software International (Pvt) Limited.
+ * All Rights Reserved.
+ *
+ * These materials are unpublished, proprietary, confidential source code of
+ * hSenid Software International (Pvt) Limited and constitute a TRADE SECRET
+ * of hSenid Software International (Pvt) Limited.
+ *
+ * hSenid Software International (Pvt) Limited retains all title
+ * to and intellectual property rights in these materials.
+ */
+package com.hSenid.dillon.springLearning.springbootlearning.service
+
+import com.hSenid.dillon.springLearning.springbootlearning.datasource.BankDataSource
+import com.hSenid.dillon.springLearning.springbootlearning.model.Bank
+import org.springframework.stereotype.Service
+
+@Service
+class BankService(private val dataSource: BankDataSource) {
+    fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
+}
