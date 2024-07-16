24d8cce269f6adc6440aea844e09e59757040e1d
dillon-sj <ds.juriansz@gmail.com>
2024-07-16 14:19:41 +0530

    chore: Add test dependency for mocking in build.gradle.kts

--- springboot-learning/springbootlearning/build.gradle.kts
+++ springboot-learning/springbootlearning/build.gradle.kts
@@ -23,9 +23,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("io.mockk:mockk:1.10.4")
+
+
 }
 
 kotlin {
