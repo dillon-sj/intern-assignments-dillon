a818c0feefa38c537436ec3f2b0b6e09a927a302
dillon-sj <ds.juriansz@gmail.com>
2024-07-19 16:24:24 +0530

    feat: Add EmployeeRepository for MongoDB collection

---
+++ springboot-learning/springBootmongoD/src/main/kotlin/com/hSenid/dillon/mongoDBSB/springBootmongoD/repository/EmployeeRepository.kt
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2008-2019 hSenid Software International (Pvt) Limited.
+ * All Rights Reserved.
+ *
+ * These materials are unpublished, proprietary, confidential source code of
+ * hSenid Software International (Pvt) Limited and constitute a TRADE SECRET
+ * of hSenid Software International (Pvt) Limited.
+ *
+ * hSenid Software International (Pvt) Limited retains all title
+ * to and intellectual property rights in these materials.
+ */
+package com.hSenid.dillon.mongoDBSB.springBootmongoD.repository
+
+import com.hSenid.dillon.mongoDBSB.springBootmongoD.model.EmployeesDocument
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface EmployeeRepository : MongoRepository<EmployeesDocument, String> {
+    // Custom query methods can be defined here if needed
+}
