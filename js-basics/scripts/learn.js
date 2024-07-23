4717e4e59db5bf601882d3ecf3e0c6d5482699a7
dillon-sj <ds.juriansz@gmail.com>
2024-07-23 13:37:04 +0530

    chore: Add lazy loading for images and update JS basics page

--- js-basics/scripts/learn.js
+++ js-basics/scripts/learn.js
@@ -1,8 +1,17 @@
-var p1 = 10;
-var p2 = 20;
-
 function additionFunc(p1, p2) {
     return console.log(p1 + p2);
 }
 
 additionFunc(5, 6);
+
+setTimeout(loadingFunc, 3000);
+
+function loadingFunc() {
+    document.getElementById("load").innerHTML = "My name is Dillon Juriansz";
+}
+
+setInterval(timeFunc, 1000);
+
+function timeFunc() {
+    document.getElementById("time").innerHTML = new Date().toLocaleTimeString();
+}
