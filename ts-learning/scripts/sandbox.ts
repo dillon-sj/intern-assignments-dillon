e75c600332cac6b2816307ecfe433905293e4440
dillon-sj <ds.juriansz@gmail.com>
2024-07-23 15:36:12 +0530

    chore: Add TypeScript files for sandbox and update HTML and CSS files

---
+++ ts-learning/scripts/sandbox.ts
@@ -0,0 +1,6 @@
+//arrays
+let names = ["John", "Doe", "Jane", "Dillon", "Vanuja"];
+console.log(names);
+
+let mixed = ["John", 1, "Doe", 2, "Jane", 3, "Dillon", 4, "Vanuja", 5];
+console.log(mixed);
