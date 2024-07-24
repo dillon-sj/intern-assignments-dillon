e8905e5cf88d1d5b980ad3f2cdaa0c004f7d9dd2
dillon-sj <ds.juriansz@gmail.com>
2024-07-24 14:05:22 +0530

    Refactor to-do list functionality and UI

--- ts-project/src/script.ts
+++ ts-project/src/script.ts
@@ -33,6 +33,35 @@ function renderTodos(): void {
     todos.forEach((todo) => {
         const li = document.createElement("li");
         li.textContent = todo.text;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete-btn");
+        deleteButton.addEventListener("click", () => deleteTodo(todo.id));
+        li.appendChild(deleteButton);
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = todo.completed;
+        checkbox.addEventListener("change", () => toggleCompletion(todo.id));
+        li.prepend(checkbox);
+
         todoList.appendChild(li);
     });
+}
+
+function deleteTodo(id: number): void {
+    const index = todos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+        todos.splice(index, 1);
+        renderTodos();
+    }
+}
+
+function toggleCompletion(id: number): void {
+    const todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+        todo.completed = !todo.completed;
+        renderTodos();
+    }
 }
