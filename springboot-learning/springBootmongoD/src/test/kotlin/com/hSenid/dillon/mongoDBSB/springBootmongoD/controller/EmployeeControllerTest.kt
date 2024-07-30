809f46f16f4f97001e37e8a1e23328cb0e446552
dillon-sj <ds.juriansz@gmail.com>
2024-07-30 11:46:56 +0530

    test: Improve error handling in EmployeeController and update EmployeeService

--- springboot-learning/springBootmongoD/src/test/kotlin/com/hSenid/dillon/mongoDBSB/springBootmongoD/controller/EmployeeControllerTest.kt
+++ springboot-learning/springBootmongoD/src/test/kotlin/com/hSenid/dillon/mongoDBSB/springBootmongoD/controller/EmployeeControllerTest.kt
@@ -3,10 +3,7 @@ package com.hSenid.dillon.mongoDBSB.springBootmongoD.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.hSenid.dillon.mongoDBSB.springBootmongoD.model.EmployeeAddress
 import com.hSenid.dillon.mongoDBSB.springBootmongoD.model.EmployeesDocument
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -14,17 +11,16 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.*
 import java.util.*
 
+
 @SpringBootTest
 @AutoConfigureMockMvc
 internal class EmployeeControllerTest @Autowired constructor(
     val mockMvc: MockMvc,
     val objectMapper: ObjectMapper,
 
-
     ) {
 
     val baseUrl = "/api/employees"
-
 
     @Nested
     @DisplayName("GET /api/employees")
@@ -33,16 +29,13 @@ internal class EmployeeControllerTest @Autowired constructor(
         @Test
         fun `should return all employees`() {
             //when
-            mockMvc.get(baseUrl)
-                .andDo { print() }
-                .andExpect {
-                    status { isOk() }
-                    content { contentType(MediaType.APPLICATION_JSON) }
-                    jsonPath("$[0].id") { value("5e4d604991b6134a75045ecd") }
+            mockMvc.get(baseUrl).andDo { print() }.andExpect {
+                status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("$[0].id") { value("5e4d604991b6134a75045ecd") }
 
-                }
+            }
         }
-
 
     }
 
@@ -57,9 +50,7 @@ internal class EmployeeControllerTest @Autowired constructor(
             val employeeId = "5e4d604991b6134a75045ecd"
 
             // when
-            mockMvc.get("$baseUrl/$employeeId")
-                .andDo { print() }
-                .andExpect { status { isOk() } }
+            mockMvc.get("$baseUrl/$employeeId").andDo { print() }.andExpect { status { isOk() } }
                 .andExpect { content { contentType(MediaType.APPLICATION_JSON) } }
 
             // then
@@ -71,9 +62,7 @@ internal class EmployeeControllerTest @Autowired constructor(
             val employeeId = "does_not_exist"
 
             // when/then
-            mockMvc.get("$baseUrl/$employeeId")
-                .andDo { print() }
-                .andExpect { status { isNotFound() } }
+            mockMvc.get("$baseUrl/$employeeId").andDo { print() }.andExpect { status { isNotFound() } }
         }
     }
 
@@ -96,10 +85,7 @@ internal class EmployeeControllerTest @Autowired constructor(
                 dob = "",
                 designation = "Deputy General Manager",
                 employee_address = EmployeeAddress(
-                    building = "No. 10",
-                    street = "Perera Avenue",
-                    city = "Nugegoda",
-                    district = "Colombo"
+                    building = "No. 10", street = "Perera Avenue", city = "Nugegoda", district = "Colombo"
                 ),
                 email = "chirantha@hsenidmobile.com",
                 mobile_number = "+94717877672",
@@ -117,15 +103,13 @@ internal class EmployeeControllerTest @Autowired constructor(
             }
 
             // then
-            performPost
-                .andDo { print() }
-                .andExpect {
-                    status { isCreated() }
-                    content {
-                        contentType(MediaType.APPLICATION_JSON)
-                        json(objectMapper.writeValueAsString(newEmployee))
-                    }
+            performPost.andDo { print() }.andExpect {
+                status { isCreated() }
+                content {
+                    contentType(MediaType.APPLICATION_JSON)
+                    json(objectMapper.writeValueAsString(newEmployee))
                 }
+            }
 
             mockMvc.get("$baseUrl/${newEmployee.id}")
                 .andExpect { content { json(objectMapper.writeValueAsString(newEmployee)) } }
@@ -143,10 +127,7 @@ internal class EmployeeControllerTest @Autowired constructor(
                 dob = "1985-03-03",
                 designation = "Deputy General Manager",
                 employee_address = EmployeeAddress(
-                    building = "No. 10",
-                    street = "Perera Avenue",
-                    city = "Nugegoda",
-                    district = "Colombo"
+                    building = "No. 10", street = "Perera Avenue", city = "Nugegoda", district = "Colombo"
                 ),
                 email = "chirantha@hsenidmobile.com",
                 mobile_number = "+94717877672",
@@ -165,24 +146,20 @@ internal class EmployeeControllerTest @Autowired constructor(
             }
 
             // then
-            performPost
-                .andDo { print() }
-                .andExpect {
-                    status { isConflict() }
-                }
-
+            performPost.andDo { print() }.andExpect {
+                status { isConflict() }
+            }
 
         }
 
-
     }
-    
-    
+
+
     @Nested
     @DisplayName("PUT /api/employees/{id}")
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     inner class PutUpdateEmployee {
-        
+
         @Test
         fun `should update the employee`() {
             //given
@@ -196,33 +173,7 @@ internal class EmployeeControllerTest @Autowired constructor(
                 dob = "1985-03-03",
                 designation = "Deputy General Manager",
                 employee_address = EmployeeAddress(
-                    building = "No. 10",
-                    street = "Perera Avenue",
-                    city = "Nugegoda",
-                    district = "Colombo"
-                ),
-                email = "chirantha@hsenidmobile.com",
-                mobile_number = "+94717877672",
-                coe = "Implementation",
-                faction = "YAKSHA",
-                sbu = listOf("SYSTEM IMPLEMENTATION"),
-                joined_date = "2008-03-03",
-                badges = emptyList()
-            )
-
-            val updatedEmployee = EmployeesDocument(
-                id = existingId,
-                employee_id = "001007",
-                employee_first_name = "Vanuja",
-                employee_last_name = "Pitigala",
-                employee_gender = "male",
-                dob = "1985-03-03",
-                designation = "Deputy General Manager",
-                employee_address = EmployeeAddress(
-                    building = "No. 10",
-                    street = "Perera Avenue",
-                    city = "Nugegoda",
-                    district = "Colombo"
+                    building = "No. 10", street = "Perera Avenue", city = "Nugegoda", district = "Colombo"
                 ),
                 email = "chirantha@hsenidmobile.com",
                 mobile_number = "+94717877672",
@@ -236,27 +187,53 @@ internal class EmployeeControllerTest @Autowired constructor(
             //when
             val performPut = mockMvc.put("/api/employees/$existingId") {
                 contentType = MediaType.APPLICATION_JSON
-                content = objectMapper.writeValueAsString(updatedEmployee)
+                content = objectMapper.writeValueAsString(existingEmployee)
             }
-            
+
             //then
-            // then
-            performPut
-                .andDo { print() }
-                .andExpect {
-                    status { isOk() }
-                    content {
-                        contentType(MediaType.APPLICATION_JSON)
-                        json(objectMapper.writeValueAsString(updatedEmployee))
-                    }
+            performPut.andDo { print() }.andExpect {
+                status { isOk() }
+                content {
+                    contentType(MediaType.APPLICATION_JSON)
+                    json(objectMapper.writeValueAsString(existingEmployee))
                 }
-            
-            
+            }
+
         }
-        
-            
-        
-    
+
+        @Test
+        fun `should return NOT FOUND if the employee id does not exist`() {
+            //given
+            val nonExistentId = "non-existent-id"
+            val updatedEmployee = EmployeesDocument(
+                id = nonExistentId,
+                employee_id = "001007",
+                employee_first_name = "Vanuja",
+                employee_last_name = "Pitigala",
+                employee_gender = "male",
+                dob = "1985-03-03",
+                designation = "Deputy General Manager",
+                employee_address = EmployeeAddress(
+                    building = "No. 10", street = "Perera Avenue", city = "Nugegoda", district = "Colombo"
+                ),
+                email = "chirantha@hsenidmobile.com",
+                mobile_number = "+94717877672",
+                coe = "Implementation",
+                faction = "YAKSHA",
+                sbu = listOf("SYSTEM IMPLEMENTATION"),
+                joined_date = "2008-03-03",
+                badges = emptyList()
+            )
+
+            //when/then
+            mockMvc.put("/api/employees/$nonExistentId") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(updatedEmployee)
+            }.andDo { print() }.andExpect {
+                    status { isNotFound() }
+                }
+        }
+
     }
 
     @Nested
@@ -264,23 +241,52 @@ internal class EmployeeControllerTest @Autowired constructor(
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     inner class DeleteExistingEmployee {
 
+        private val employeeId = "to-be-deleted"
+        private val nonExistentId = "non-existent-id"
+
+        @BeforeEach
+        fun setup() {
+            val employeeToDelete = EmployeesDocument(
+                id = employeeId,
+                employee_id = "001207",
+                employee_first_name = "Vanuja",
+                employee_last_name = "Pitigala",
+                employee_gender = "male",
+                dob = "1985-03-03",
+                designation = "Deputy General Manager",
+                employee_address = EmployeeAddress(
+                    building = "No. 10", street = "Perera Avenue", city = "Nugegoda", district = "Colombo"
+                ),
+                email = "vanuja@hsenidmobile.com",
+                mobile_number = "+94717877672",
+                coe = "Implementation",
+                faction = "YAKSHA",
+                sbu = listOf("SYSTEM IMPLEMENTATION"),
+                joined_date = "2008-03-03",
+                badges = emptyList()
+            )
+            mockMvc.post(baseUrl) {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(employeeToDelete)
+            }
+        }
+
         @Test
         fun `should delete employee with given employee number`() {
-            //given
-            val employeeId = "5e4d608291b6134a75045ecf"
+            // when/then
+            mockMvc.delete("$baseUrl/$employeeId").andDo { print() }.andExpect {
+                status { isNoContent() }
+            }
 
+            mockMvc.get("$baseUrl/$employeeId").andExpect { status { isNotFound() } }
+        }
 
-            //when/then
-            mockMvc.delete("$baseUrl/$employeeId")
-                .andDo { print() }
-                .andExpect {
-                    status { isNoContent() }
-                }
-
-            mockMvc.get("$baseUrl/$employeeId")
-                .andExpect { status { isNotFound() } }
-
-
+        @Test
+        fun `should return NOT FOUND if the employee id does not exist`() {
+            // when/then
+            mockMvc.delete("$baseUrl/$nonExistentId").andDo { print() }.andExpect {
+                status { isNotFound() }
+            }
         }
 
     }
