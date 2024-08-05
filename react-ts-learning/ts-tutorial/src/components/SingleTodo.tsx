cda005f7bdcb105fdbc27bb93decc7ebe948d080
dillon-sj <ds.juriansz@gmail.com>
2024-08-05 14:48:24 +0530

    chore: Add edit functionality to SingleTodo component DONE basic

--- react-ts-learning/ts-tutorial/src/components/SingleTodo.tsx
+++ react-ts-learning/ts-tutorial/src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Todo } from "../model";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -11,6 +11,9 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
     const handleDone = (id: number) => {
         setTodos(
             todos.map((todo) =>
@@ -19,19 +22,57 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
         );
     };
 
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+    const handleEdit = (e: React.FormEvent, id: number) => {
+        e.preventDefault();
+
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, todo: editTodo } : todo
+            )
+        );
+        setEdit(false);
+    };
+
+    const InputRef = useRef<HTMLInputElement>(null);
+
     return (
         <div>
-            <form className="todosSingle">
-                {todo.isDone ? (
+            <form
+                className="todosSingle"
+                onSubmit={(e) => handleEdit(e, todo.id)}
+            >
+                {edit ? (
+                    <input
+                        ref={InputRef}
+                        value={editTodo}
+                        onChange={(e) => setEditTodo(e.target.value)}
+                        className="todosSingleText"
+                    />
+                ) : todo.isDone ? (
                     <s className="todosSingleText">{todo.todo}</s>
                 ) : (
                     <span className="todosSingleText">{todo.todo}</span>
                 )}
+
                 <div>
-                    <span className="icon">
+                    <span
+                        className="icon"
+                        onClick={() => {
+                            if (!edit && !todo.isDone) {
+                                setEdit(!edit);
+                            }
+                        }}
+                    >
                         <AiFillEdit />
                     </span>
-                    <span className="icon">
+                    <span
+                        className="icon"
+                        onClick={() => handleDelete(todo.id)}
+                    >
                         <AiFillDelete />
                     </span>
                     <span className="icon" onClick={() => handleDone(todo.id)}>
--- react-ts-learning/ts-tutorial/src/model.ts
+++ react-ts-learning/ts-tutorial/src/model.ts
@@ -3,3 +3,19 @@ export interface Todo {
     todo: string;
     isDone: boolean;
 }
+
+// type TodoAction =
+//     | {type: "ADD_TODO", payload: string}
+//     | {type: "REMOVE_TODO", payload: number}
+//     | {type: "DONE_TODO", payload: number}
+
+// const TodoReducer = (state: Todo[], action: TodoAction) => {
+
+// };
+
+// import { useReducer } from "react";
+// const ReucerExample = () => {
+//     const [state, dispatch] = useReducer(TodoReducer, []);
+//     return ();
+
+// };
